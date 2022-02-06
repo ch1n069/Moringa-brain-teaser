b470da81b489b355ddf79fdf782f8c82538c9d4d
Moringa School <brunolalpachino@gmail.com>
2022-02-06 23:38:23 +0300

    experienced bugs with the displaying of score at the end of the quiz

--- game.js
+++ game.js
@@ -1,17 +1,17 @@
 var question001 = ["What is html ?"];
-var choices001 = ["<input onclick=correct001() name=button001 type=radio />2<br /><input onclick=incorrect001() name=button001 type=radio />4<br /><input onclick=incorrect001() name=button001 type=radio />7<br />"];
+var choices001 = ["<input onclick=correct001() name=button001 type=radio />Hyper text mark up language<br /><input onclick=incorrect001() name=button001 type=radio />css<br /><input onclick=incorrect001() name=button001 type=radio />text languge<br />"];
 
 
 var question002 = ["What is html ?"];
-var choices002 = ["<input onclick=correct002() name=button002 type=radio />2<br /><input onclick=incorrect001() name=button002 type=radio />4<br /><input onclick=incorrect002() name=button002 type=radio />7<br />"];
+var choices002 = ["<input onclick=correct002() name=button002 type=radio />Hyper text markup language <br /><input onclick=incorrect001() name=button002 type=radio />Cscading stylesheet<br /><input onclick=incorrect002() name=button002 type=radio />DOm<br />"];
 
 
 var question003 = ["What is html ?"];
-var choices003 = ["<input onclick=correct003() name=button003 type=radio />2<br /><input onclick=incorrect003() name=button003 type=radio />4<br /><input onclick=incorrect003() name=button003 type=radio />7<br />"];
+var choices003 = ["<input onclick=correct003() name=button003 type=radio />Hyper-text-markup-language<br /><input onclick=incorrect003() name=button003 type=radio />fork<br /><input onclick=incorrect003() name=button003 type=radio />css<br />"];
 
 
 var question004 = ["What is html ?"];
-var choices004 = ["<input onclick=correct004() name=button004 type=radio />2<br /><input onclick=incorrect004() name=button004 type=radio />4<br /><input onclick=incorrect004() name=button004 type=radio />7<br />"];
+var choices004 = ["<input onclick=correct004() name=button004 type=radio />Hyper text<br /><input onclick=incorrect004() name=button004 type=radio />garden<br /><input onclick=incorrect004() name=button004 type=radio />php<br />"];
 
 var a;
 var b;
@@ -54,7 +54,7 @@ function set003() {
     c = 0;
     message.innerHTML = question003;
     option.innerHTML = choices003;
-    click.innerHTML = "<button onclick=result001()>Score</button>";
+    click.innerHTML = "<button onclick=result001()>Repeat</button>";
 
 }
 
@@ -66,10 +66,16 @@ function incorrect003() {
     c = 0;
 }
 
+
+//this is the result function //
 function result001() {
     z = a + b + c;
     message.innerHTML = "End of quiz";
+    //makes the  fields dissapear
     options.innerHTML = "";
     comment.innerHTML = "your score is " + z;
+    click001.innerHTML = "<button onclick=result001()>repeat</button>"
+
+
 
 }
