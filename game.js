1549964db05f16cb70599ac71412e5c637be2481
Moringa School <brunolalpachino@gmail.com>
2022-02-06 22:21:02 +0300

    added our questions and choice using variables

---
+++ game.js
@@ -0,0 +1,19 @@
+var question001 = ["What is html ?"];
+var choices001 = ["<input onclick=correct001() name=button001 type=radio />2<br /><input onclick=incorrect001() name=button001 type=radio />4<br /><input onclick=incorrect001() name=button001 type=radio />7<br />"];
+
+
+var question002 = ["What is html ?"];
+var choices002 = ["<input onclick=correct002() name=button002 type=radio />2<br /><input onclick=incorrect001() name=button001 type=radio />4<br /><input onclick=incorrect002() name=button002 type=radio />7<br />"];
+
+
+var question003 = ["What is html ?"];
+var choices003 = ["<input onclick=correct003() name=button003 type=radio />2<br /><input onclick=incorrect003() name=button003 type=radio />4<br /><input onclick=incorrect003() name=button001 type=radio />7<br />"];
+
+
+var question004 = ["What is html ?"];
+var choices004 = ["<input onclick=correct004() name=button004 type=radio />2<br /><input onclick=incorrect004() name=button004 type=radio />4<br /><input onclick=incorrect004() name=button004 type=radio />7<br />"];
+
+var a;
+var b;
+var c;
+var z
