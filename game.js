5a5bbd56aa58ac1ddd2b51e4c1a42b9262e30021
Moringa School <brunolalpachino@gmail.com>
2022-02-08 17:14:22 +0300

    ft== grabbed our form elements

--- game.js
+++ game.js
@@ -1,2 +1,10 @@
 const container1 = document.querrySelector('.contianer');
-const form = document.querry
+const form = document.querrySelector('.contianer');
+const form = document.querrySelector('#quiz-form')
+const question = document.querrySelector('.quiz')
+const option = document.querrySelector('.all-options');
+const buttonEl = document.querySelector('.button');
+const scoreEl = document.querySelector('.scoreBoard .score-num ')
+const answerEl = document.querySelector('.scoreBoard .answered-num')
+
+// grabbing of our elements for the from above
