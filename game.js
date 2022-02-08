1ce15f82c928bcf03b290a958d776336923c2daa
Moringa School <brunolalpachino@gmail.com>
2022-02-08 20:44:51 +0300

    ft== added our questions array with answers

--- game.js
+++ game.js
@@ -1,40 +1,16 @@
-const baseURL = 'https://opentdb.com/api.php?amount=5';
+const quizData = [{
+        question: "What is html ?",
 
-// added a url to generate our questions
-const container1 = document.querrySelector('.contianer');
-const form = document.querrySelector('.contianer');
-const form = document.querrySelector('#quiz-form')
-const question = document.querrySelector('.quiz')
-const option = document.querrySelector('.all-options');
-const buttonEl = document.querySelector('.button');
-const scoreEl = document.querySelector('.scoreBoard .score-num ')
-const answerEl = document.querySelector('.scoreBoard .answered-num')
+        a: "text",
+        b: "hyper text markup language ",
+        c: "mark language",
+        d: "programming language",
+        correct: "b",
+    },
+    {
+        question: "What is css ?",
+        a: "cascading stylesheet"
+        b: ""
 
-// grabbing of our elements for the from above
-
-let question, answer;
-let options = []
-let score = 0;
-let answeredQues = 0;
-
-window.addEventListener('DOMContentLoaded', quizApp);
-
-
-// creating of quiz function
-
-function quizApp() {
-    const data = fetchQuiz();
-    question = data
-
-
-
-}
-
-async function fetchQuiz() {
-    const response = await fetch(baseURL);
-    const data = await response.json();
-
-    return data.results;
-
-
-}
+    }
+];
