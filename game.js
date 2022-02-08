de127c4c435513e24a33b68dbd8e12f261a3f61a
Moringa School <brunolalpachino@gmail.com>
2022-02-08 21:12:29 +0300

    ft== defined functions to display questions and answers to user

--- game.js
+++ game.js
@@ -43,3 +43,41 @@ const quiz = document.getElementById('quiz')
 const answerEl = document.querySelectorAll('.answer')
 const questionEl = document.getElementById('question')
 const a_text = document.getElementById('a-text')
+const b_text = document.getElementById('b-text')
+const c_text = document.getElementById('c-text')
+const d_text = document.getElementById('d-text')
+
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz()
+
+
+function loadQuiz() {
+    deselectAnswers()
+
+    const currentQuizData = quizData[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
+
+function deselectAnswers() {
+    answerEl.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected() {
+    let answer
+    answerEl.forEach(answerEl => {
+
+        if (answerEl.checked) {
+            answer = answerEl.id
+        }
+
+    })
+    return answer
+}
