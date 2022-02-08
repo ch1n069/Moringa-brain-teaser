213de8dac9f76521828e3cc64d4f3c0ba8ef9b3e
Moringa School <brunolalpachino@gmail.com>
2022-02-08 20:48:17 +0300

    bug === fixed typo error on our question array

--- game.js
+++ game.js
@@ -9,8 +9,31 @@ const quizData = [{
     },
     {
         question: "What is css ?",
-        a: "cascading stylesheet"
-        b: ""
+        a: "cascading stylesheet",
+        b: " programming language",
+        c: "A scripting language",
+        d: "An editor language",
+        correct: "a",
 
+    },
+    {
+        question: "Where is the link to the stylesheet placed ?",
+        a: "in the footer",
+        b: "inside the body of the html ",
+        c: "at the header section near the title",
+        d: "inside a class",
+        correct: "c",
+
+    },
+    {
+        {
+            question: "Where is the link to the stylesheet placed ?",
+            a: "in the footer",
+            b: "inside the body of the html ",
+            c: "at the header section near the title",
+            d: "inside a class",
+            correct: "c",
+
+        }
     }
 ];
