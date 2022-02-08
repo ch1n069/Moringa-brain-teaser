991695bc06b9dcc16c17b2851e930eef97f4e3ff
Moringa School <brunolalpachino@gmail.com>
2022-02-08 17:42:24 +0300

    funct== added the api to generate our quizes && defined functions for fetching of the data

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+const baseURL = 'https://opentdb.com/api.php?amount=5';
+
+// added a url to generate our questions
 const container1 = document.querrySelector('.contianer');
 const form = document.querrySelector('.contianer');
 const form = document.querrySelector('#quiz-form')
@@ -8,3 +11,30 @@ const scoreEl = document.querySelector('.scoreBoard .score-num ')
 const answerEl = document.querySelector('.scoreBoard .answered-num')
 
 // grabbing of our elements for the from above
+
+let question, answer;
+let options = []
+let score = 0;
+let answeredQues = 0;
+
+window.addEventListener('DOMContentLoaded', quizApp);
+
+
+// creating of quiz function
+
+function quizApp() {
+    const data = fetchQuiz();
+    question = data
+
+
+
+}
+
+async function fetchQuiz() {
+    const response = await fetch(baseURL);
+    const data = await response.json();
+
+    return data.results;
+
+
+}
